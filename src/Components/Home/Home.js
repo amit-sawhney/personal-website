a1444d436065aaf786f8a55c742e2177f6c6d813
Amit Sawhney <amit.sawhney2002@gmail.com>
2020-12-12 01:22:08 -0600

    added experience and projects sections

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import avatar from '../../assets/avatar.svg'
-import { Avatar } from '@material-ui/core'
-import './Home.scss'
+import avatar from '../../assets/avatar.svg';
+import github from '../../assets/github.svg';
+import instagram from '../../assets/instagram.svg';
+import linkedin from '../../assets/linkedin.svg';
+import { Avatar, Link } from '@material-ui/core';
+import './Home.scss';
 
 const Home = (props) => {
 
@@ -9,12 +12,35 @@ const Home = (props) => {
         <div className="content-container">
             <div className="sidebar">
                 <Avatar className="avatar" src={avatar} />
-                <p>test</p>
+                <div className="socials">
+                    <Link rel="noopener" target="_blank" href="https://github.com/amit-sawhney">
+                        <Avatar className="social-avatar" src={github} />
+                    </Link>
+                    <Link rel="noopener" target="_blank" href="https://www.linkedin.com/in/amit-m-sawhney/">
+                        <Avatar variant="square" className="social-avatar" src={linkedin} />
+                    </Link>
+                    <Link rel="noopener" target="_blank" href="https://www.instagram.com/amit__02/">
+                        <Avatar variant="square" className="social-avatar" src={instagram} />
+                    </Link>
+                </div>
+                <p>{`Computer Science + Math @ UIUC May '24`}</p>
+                <p>{`James Scholar`}</p>
+                <p>{`GPA: 4.0 / 4.0`}</p>
+                <p></p>
             </div>
             <div className="content">
-                <h2>content</h2>
+                <div className="content-section">
+                    <text id="header">Experience</text>
+                    <div className="experience-item">
+                        <text id="company"></text>
+                        <text id="title"></text>
+                    </div>
+                </div>
+                <div className="content-section">
+                        <text id="header">Projects</text>
+                    </div>
+                </div>
             </div>
-        </div>
     );
 }
 
