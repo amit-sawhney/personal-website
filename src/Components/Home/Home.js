7b6006a5175f8ac42902058f0c9e342d1bc8fe17
Amit Sawhney <amit.sawhney2002@gmail.com>
2020-12-12 16:50:19 -0600

    added sidebar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,44 +3,74 @@ import avatar from '../../assets/avatar.svg';
 import github from '../../assets/github.svg';
 import instagram from '../../assets/instagram.svg';
 import linkedin from '../../assets/linkedin.svg';
-import { Avatar, Link } from '@material-ui/core';
+import statefarm from '../../assets/statefarm.jpg';
+import hack4impact from '../../assets/hack4impact.png';
+import { Avatar, Drawer, Link, Toolbar } from '@material-ui/core';
 import './Home.scss';
 
 const Home = (props) => {
 
     return (
         <div className="content-container">
-            <div className="sidebar">
-                <Avatar className="avatar" src={avatar} />
-                <div className="socials">
-                    <Link rel="noopener" target="_blank" href="https://github.com/amit-sawhney">
-                        <Avatar className="social-avatar" src={github} />
-                    </Link>
-                    <Link rel="noopener" target="_blank" href="https://www.linkedin.com/in/amit-m-sawhney/">
-                        <Avatar variant="square" className="social-avatar" src={linkedin} />
-                    </Link>
-                    <Link rel="noopener" target="_blank" href="https://www.instagram.com/amit__02/">
-                        <Avatar variant="square" className="social-avatar" src={instagram} />
-                    </Link>
+            <Drawer
+                className="drawer"
+                variant="permanent"
+                classes={{
+                    paper: "drawerPaper",
+                }}
+            >
+                <Toolbar />
+                <div className="drawerContainer">
+                    <div className="sidebar">
+                        <Avatar className="avatar" src={avatar} />
+                        <div className="socials">
+                            <Link rel="noopener" target="_blank" href="https://github.com/amit-sawhney">
+                                <Avatar className="social-avatar" src={github} />
+                            </Link>
+                            <Link rel="noopener" target="_blank" href="https://www.linkedin.com/in/amit-m-sawhney/">
+                                <Avatar variant="square" className="social-avatar" src={linkedin} />
+                            </Link>
+                            <Link rel="noopener" target="_blank" href="https://www.instagram.com/amit__02/">
+                                <Avatar variant="square" className="social-avatar" src={instagram} />
+                            </Link>
+                        </div>
+                        <p>{`Computer Science + Math @ UIUC May '24`}</p>
+                        <p>{`James Scholar`}</p>
+                        <p>{`GPA: 4.0 / 4.0`}</p>
+                    </div>
                 </div>
-                <p>{`Computer Science + Math @ UIUC May '24`}</p>
-                <p>{`James Scholar`}</p>
-                <p>{`GPA: 4.0 / 4.0`}</p>
-                <p></p>
-            </div>
+            </Drawer>
+
             <div className="content">
                 <div className="content-section">
                     <text id="header">Experience</text>
                     <div className="experience-item">
-                        <text id="company"></text>
-                        <text id="title"></text>
+                        <div className="title">
+                            <img width="50px" src={statefarm} alt="State Farm logo" />
+                            <p id="company"> State Farm | Research Development Center Intern (Spring 2021)</p>
+                        </div>
                     </div>
-                </div>
-                <div className="content-section">
-                        <text id="header">Projects</text>
+                    <div className="experience-item">
+                        <div className="title">
+                            <img width="50px" src={hack4impact} alt="State Farm logo" />
+                            <p id="company"> Hack4Impact | Software Developer (Fall 2020 - Present)</p>
+                        </div>
+                    </div>
+                    <div className="experience-item">
+                        <div className="title">
+                            <img width="50px" src={statefarm} alt="State Farm logo" />
+                            <p id="company"> State Farm | Enterprise Technology Management Services Intern (Summer 2020)</p>
+                        </div>
+                    </div>
+                    <div className="experience-item">
+                        <div className="title">
+                            <img width="50px" src={statefarm} alt="State Farm logo" />
+                            <p id="company"> State Farm | Enterprise Technology Test Data Management Intern (Summer 2019)</p>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
     );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react';
-
+import { AppBar, Toolbar } from '@material-ui/core';
 import './Navbar.scss';
 import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
 
     return (
-        <nav className="navbar-content">
-            <Link style={{ flexGrow: 1 }} id="nav-item" to="/">Amit Sawhney</Link>
-            <Link id="nav-item" to="/blog">Blog</Link>
-            <Link id="nav-item" to="/">Contact</Link>
-
-        </nav>
+        <AppBar elevation={0} className="navbar" position="sticky">
+            <Toolbar className="navbar-content">
+                    <Link style={{ flexGrow: 1 }} id="nav-header" to="/">Amit Sawhney</Link>
+                    <Link id="nav-item" to="/blog">Blog</Link>
+                    <Link id="nav-item" to="/">Contact</Link>
+            </Toolbar>
+        </AppBar>
 
     );
 }
