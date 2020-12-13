b451ac7387b52b026f29d4d857b5cbb6bda44c2e
Amit Sawhney <amit.sawhney2002@gmail.com>
2020-12-13 01:25:37 -0600

    added hover transitions for projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -3,6 +3,9 @@ import './Projects.scss';
 import { Divider, Grid, Box } from '@material-ui/core';
 import studentsmeet from '../../assets/studentsmeet.png';
 import planethunters from '../../assets/planethunters.png';
+import personalwebsite from '../../assets/personalwebsite.png';
+import dashboard from '../../assets/dashboard.png';
+import statefarm from '../../assets/statefarm.jpg';
 
 const Projects = (props) => {
 
@@ -13,18 +16,69 @@ const Projects = (props) => {
             <div className="grid">
                 <div className="project">
                     <img src={studentsmeet} className="project-preview"></img>
+                    <div className="overlay">
+                        <div>
+                            <h3 style={{marginBottom: 0}}>Students Meet</h3>
+                        </div>
+                        <div>
+                            <p style={{marginTop: 0}}>Hackathon</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="project">
                     <img src={planethunters} className="project-preview"></img>
+                    <div className="overlay">
+                        <div>
+                            <h3 style={{marginBottom: 0}}>Planet Hunters</h3>
+                        </div>
+                        <div>
+                            <p style={{marginTop: 0}}>Class ML/DL Project</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="project">
-                    
+                    <img src={personalwebsite} className="project-preview"></img>
+                    <div className="overlay">
+                        <div>
+                            <h3 style={{marginBottom: 0}}>Personal Website</h3>
+                        </div>
+                        <div>
+                            <p style={{marginTop: 0}}>Side Project</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="project">
-                    
+                    <img src={dashboard} className="project-preview"></img>
+                    <div className="overlay">
+                        <div>
+                            <h3 style={{marginBottom: 0}}>3DP4ME Dashboard</h3>
+                        </div>
+                        <div>
+                            <p style={{marginTop: 0}}>Hack4Impact Fall 2020</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="project">
-                    
+                    <img src={statefarm} className="project-preview"></img>
+                    <div className="overlay">
+                        <div>
+                            <h3 style={{marginBottom: 0}}>Technical Recovery Plan</h3>
+                        </div>
+                        <div>
+                            <p style={{marginTop: 0}}>State Farm Summer 2020</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={statefarm} className="project-preview"></img>
+                    <div className="overlay">
+                        <div>
+                            <h3 style={{marginBottom: 0}}>DropOff/TDM Exception</h3>
+                        </div>
+                        <div>
+                            <p style={{marginTop: 0}}>State Farm Summer 2019</p>
+                        </div>
+                    </div>
                 </div>
 
             </div>
