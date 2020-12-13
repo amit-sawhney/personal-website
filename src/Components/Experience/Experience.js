3f01d6394bd11ab765922970942696b5410f61d1
Amit Sawhney <amit.sawhney2002@gmail.com>
2020-12-12 22:03:46 -0600

    edited experience styling

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -12,12 +12,12 @@ const Experience = (props) => {
             <h1 className="experience-header">Work Experience</h1>
             <Divider />
             <VerticalTimeline className="timeline">
-            <VerticalTimelineElement
+                <VerticalTimelineElement
                     className="vertical-timeline-element--work"
                     contentStyle={{ background: '#7510f7', color: '#fff' }}
                     contentArrowStyle={{ borderRight: '7px solid rgb(117,16,247)' }}
                     dateClassName="timeline-date"
-                    iconStyle={{ background: '#F77510', color: '#fff' }}
+                    iconStyle={{ background: '#86A00F', color: '#fff' }}
                     icon={<WorkIcon />}
                 >
                     <h2 className="timeline-position">ET Software Developer Intern</h2>
@@ -26,12 +26,12 @@ const Experience = (props) => {
                     <em>Summer 2021</em>
                     <p>Looking to the future...</p>
                 </VerticalTimelineElement>
-            <VerticalTimelineElement
+                <VerticalTimelineElement
                     className="vertical-timeline-element--work"
                     contentStyle={{ background: '#7510f7', color: '#fff' }}
                     contentArrowStyle={{ borderRight: '7px solid rgb(117,16,247)' }}
                     dateClassName="timeline-date"
-                    iconStyle={{ background: '#F77510', color: '#fff' }}
+                    iconStyle={{ background: '#86A00F', color: '#fff' }}
                     icon={<WorkIcon />}
                 >
                     <h2 className="timeline-position">Software Developer</h2>
@@ -45,59 +45,60 @@ const Experience = (props) => {
                     contentStyle={{ background: '#7510f7', color: '#fff' }}
                     contentArrowStyle={{ borderRight: '7px solid rgb(117,16,247)' }}
                     dateClassName="timeline-date"
-                    iconStyle={{ background: '#F77510', color: '#fff' }}
+                    iconStyle={{ background: '#86A00F', color: '#fff' }}
                     icon={<WorkIcon />}
                 >
                     <h2 className="timeline-position">Research Dev. Center Intern</h2>
                     <h2 className="timeline-title">State Farm</h2>
                     <h4 className="timeline-subtitle">Urbana, IL</h4>
                     <em>Spring 2021</em>
-                    <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+                    <p>Looking to the future...</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
                     contentStyle={{ background: '#7510f7', color: '#fff' }}
                     contentArrowStyle={{ borderRight: '7px solid rgb(117,16,247)' }}
                     dateClassName="timeline-date"
-                    iconStyle={{ background: '#F77510', color: '#fff' }}
-                    icon={<WorkIcon />}
-                >
-                    <h2 className="timeline-position">ET Management Services Intern</h2>
-                    <h2 className="timeline-title">State Farm</h2>
-                    <h4 className="timeline-subtitle">Bloomington, IL</h4>
-                    <em>Summer 2020</em>
-                    <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{ background: '#7510f7', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid rgb(117,16,247)' }}
-                    dateClassName="timeline-date"
-                    iconStyle={{ background: '#F77510', color: '#fff' }}
+                    iconStyle={{ background: '#86A00F', color: '#fff' }}
                     icon={<WorkIcon />}
                 >
                     <h2 className="timeline-position">Software Developer</h2>
                     <h2 className="timeline-title">Hack4Impact</h2>
                     <h4 className="timeline-subtitle">Urbana, IL</h4>
                     <em>Fall 2020</em>
-                    <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+                    <p>Developed a dashboard that allows the 3DP4ME nonprofit increase the design and distribution of 3D printed hearing
+                        aids to low income citizens of Jordan</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
                     contentStyle={{ background: '#7510f7', color: '#fff' }}
                     contentArrowStyle={{ borderRight: '7px solid rgb(117,16,247)' }}
                     dateClassName="timeline-date"
-                    iconStyle={{ background: '#F77510', color: '#fff' }}
+                    iconStyle={{ background: '#86A00F', color: '#fff' }}
+                    icon={<WorkIcon />}
+                >
+                    <h2 className="timeline-position">ET Management Services Intern</h2>
+                    <h2 className="timeline-title">State Farm</h2>
+                    <h4 className="timeline-subtitle">Bloomington, IL</h4>
+                    <em>Summer 2020</em>
+                    <p>Developed a Technical Recovery Plan application that supported application backups</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: '#7510f7', color: '#fff' }}
+                    contentArrowStyle={{ borderRight: '7px solid rgb(117,16,247)' }}
+                    dateClassName="timeline-date"
+                    iconStyle={{ background: '#86A00F', color: '#fff' }}
                     icon={<WorkIcon />}
                 >
                     <h2 className="timeline-position">ET Test Data Management Intern</h2>
                     <h2 className="timeline-title">State Farm</h2>
                     <h4 className="timeline-subtitle">Bloomington, IL</h4>
                     <em>Summer 2019</em>
-                    <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+                    <p>Developed Dropoff, an application that tracks and manages form approval through company management</p>
                 </VerticalTimelineElement>
-                
-                
+
+
             </VerticalTimeline>
         </div >
     )
